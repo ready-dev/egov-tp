99d81452f157d443c9b640b451badda11187339d
cherryblast <95.cherryblast@gmail.com>
2020-03-06 09:28:17 +0900

    백업

--- js/common.js
+++ js/common.js
@@ -1,12 +1,1 @@
-//2단 메뉴 함수
-$(document).ready(function(){
-        // memu 클래스 바로 하위에 있는 a 태그를 클릭했을때
-        $(".menu>a").click(function(){
-            // 현재 클릭한 태그가 a 이기 때문에
-            // a 옆의 태그중 ul 태그에 hide 클래스 태그를 넣던지 빼던지 한다.
-            $(this).next("ul").toggleClass("hide");
-        });
-    });
-
-
-
+$(function(){ $("#accordian h3").click(function(){ $("#accordian ul ul").slideUp(); if(!$(this).next().is(":visible")) { $(this).next().slideDown(); } }) })
